4dbccaf68df78651f294bc9a170a91ae452b894f
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-06-08 19:50:11 -0500

    Comentario

---
+++ src/1_hola_mundo2.py
@@ -0,0 +1,18 @@
+"""
+Uso: Comentario
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 06 Junio 2021
+"""
+
+#Comentario de una sola linea
+
+"""
+Comentario
+de
+varias
+lineas
+"""
+
+print("Hola Mundo!")
