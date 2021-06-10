879928c9cda5244065bd0360da5ef269765d366f
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-06-09 23:31:37 -0500

    Acercamiento con las variables

---
+++ src/2_variables2.py
@@ -0,0 +1,18 @@
+"""
+Uso: Acercamiento con los identificadores y variables
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 09 Junio 2021
+"""
+
+variable_texto = "Hola"
+variable_texto2 = "Mundo"
+variable_texto3 = "!"
+
+print(variable_texto)
+print(variable_texto2)
+print(variable_texto3)
+
+
+
