4b9780e831ae670e64afa67d54d25e5b47c097ed
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-06-09 23:31:47 -0500

    Acercamiento con los identificadores y variables

---
+++ src/2_variables3.py
@@ -0,0 +1,25 @@
+"""
+Uso: Acercamiento con los identificadores y variables
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 09 Junio 2021
+"""
+
+variable_texto, variable_texto2, variable_texto3 = "Hola", "Mundo", "!"
+ 
+variable_texto4 = """lorem impuslelorem impusle
+lorem impuslelorem impuslelorem impusle
+lorem impusle
+lorem impusle
+lorem impuslelorem impusle
+"""
+
+print(variable_texto + variable_texto2 + variable_texto3)
+print(variable_texto +" "+ variable_texto2 + variable_texto3)
+print("")
+print(variable_texto, variable_texto2, variable_texto3)
+print("")
+print(variable_texto4)
+
+
