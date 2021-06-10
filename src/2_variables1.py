64557e2e7e7736c606691b55f80d080a6c7e5d96
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-06-09 23:31:15 -0500

    Variables

---
+++ src/2_variables1.py
@@ -0,0 +1,17 @@
+"""
+Uso: Acercamiento con los identificadores y variables
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 09 Junio 2021
+"""
+
+variabletexto = "Hola"
+variableTexto = "Mundo"
+VARIABLETEXTO = "!"
+
+print(variabletexto)
+print(variableTexto)
+print(VARIABLETEXTO)
+
+
