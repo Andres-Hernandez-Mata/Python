30ba4624b90b9f8c8e126cc9f0e1a8b2e9382630
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-06-14 19:42:35 -0500

    Lectura de datos desde consola 4

---
+++ src/3_lectura4.py
@@ -0,0 +1,18 @@
+"""
+Uso: Lectura de datos desde consola
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 14 Junio 2021
+"""
+
+import sys
+
+nombre,edad = sys.argv[1], sys.argv[2]
+
+print("Hola "+ nombre + ", tienes " + edad + " anios")
+
+print(sys.argv)
+
+
+
