755fca53750387ac7e6914aaec4f6524aed7161d
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-06-14 19:42:04 -0500

    Lectura de datos desde consola 2

---
+++ src/3_lectura2.py
@@ -0,0 +1,12 @@
+"""
+Uso: Lectura de datos desde consola
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 14 Junio 2021
+"""
+
+nombre = input("Nombre: ")
+edad = input("Edad: ")
+
+print("Hola "+ nombre + ", tienes " + edad + " anios")
