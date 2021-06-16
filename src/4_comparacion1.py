05f31d27cb7a38bf6894b65dcd66ef999db46d43
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-06-15 21:46:17 -0500

    Comparacion 1

---
+++ src/4_comparacion1.py
@@ -0,0 +1,26 @@
+"""
+Uso: Comparacion 1
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 15 Junio 2021
+"""
+
+i, j = 4, 5
+print(i>j)
+
+i, j = 4, 5
+print(i>=j)
+
+i, j = 4, 5
+print(i<j)
+
+i, j = 4, 5
+print(i<=j)
+
+i, j = 4, 5
+print(i==j)
+
+i, j = 4, 5
+print(i!=j)
+
