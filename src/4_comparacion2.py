72c0d4e8a48cb8936ccfd3057d16f1dbf3666004
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-06-15 21:46:38 -0500

    Comparacion 2

---
+++ src/4_comparacion2.py
@@ -0,0 +1,27 @@
+"""
+Uso: Comparacion 2
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 15 Junio 2021
+"""
+
+calif_parcial = int( input("Parcial: ") )
+CALIF_APROB = 70
+
+print( calif_parcial, type(calif_parcial) )
+print( CALIF_APROB,   type(CALIF_APROB)   )
+
+print(calif_parcial > CALIF_APROB)
+
+print( str(calif_parcial) + " > "  + str(CALIF_APROB) + " = ", calif_parcial > CALIF_APROB)
+print( str(calif_parcial) + " >= " + str(CALIF_APROB) + " = ", calif_parcial >= CALIF_APROB)
+print( str(calif_parcial) + " < "  + str(CALIF_APROB) + " = ", calif_parcial <  CALIF_APROB)
+print( str(calif_parcial) + " <= " + str(CALIF_APROB) + " = ", calif_parcial <= CALIF_APROB)
+print( str(calif_parcial) + " == " + str(CALIF_APROB) + " = ", calif_parcial == CALIF_APROB)
+print( str(calif_parcial) + " != " + str(CALIF_APROB) + " = ", calif_parcial != CALIF_APROB)
+
+
+
+
+
