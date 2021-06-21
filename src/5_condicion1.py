ee655510ac5f5d47274571dad59b560b96628938
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-06-21 06:27:24 -0500

    Condicional 1

---
+++ src/5_condicion1.py
@@ -0,0 +1,14 @@
+"""
+Uso: Condicional 1
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 21 Junio 2021
+"""
+
+calificacion = int(input("Calificacion: "))
+CALIF_APROBATORIA = 70
+
+if(calificacion >= CALIF_APROBATORIA):
+	print("Aprobado!")
+
