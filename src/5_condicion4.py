a8672028055cadbc85110f4b8bd859c6f40297c5
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-06-21 06:27:50 -0500

    Condicional 4

---
+++ src/5_condicion4.py
@@ -0,0 +1,18 @@
+"""
+Uso: Condicional 4
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 21 Junio 2021
+"""
+
+calificacion = int(input("Calificacion: "))
+CALIF_APROBATORIA = 70
+
+if(calificacion >= 0 and calificacion <= 100):
+	if(calificacion > CALIF_APROBATORIA):
+		print("Aprobado!")
+	elif(calificacion == CALIF_APROBATORIA):
+		print("Aprobado, justo en el limite de la calificación aprobatoria!")
+	else:
+		print("Reprobado!")
