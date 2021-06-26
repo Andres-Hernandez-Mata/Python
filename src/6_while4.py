09c0a2b21e6081f19e537e6e101d5dda08ba5ac3
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-06-25 19:38:20 -0500

    Condicional While 4

---
+++ src/6_while4.py
@@ -0,0 +1,19 @@
+"""
+Uso: Condicional While 4
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 25 Junio 2021
+"""
+
+
+ini_serie = 0
+cont_serie = ini_serie
+fin_serie = 10 
+
+while cont_serie < fin_serie:
+	print(cont_serie)
+	cont_serie = cont_serie + 1
+
+print("Termine de mostrar la serie del 0 a 9")
+
