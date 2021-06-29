d349f00b76585cfb09e2a5ac0f811bc71aba88ae
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-06-29 18:26:04 -0500

    Condicional For 2

---
+++ src/7_for2.py
@@ -0,0 +1,17 @@
+"""
+Uso: Condicional For 2
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 29 Junio 2021
+"""
+
+ini_serie = 0
+cont_serie = ini_serie
+fin_serie = 10 
+
+while cont_serie < fin_serie:
+	print(cont_serie)
+	cont_serie = cont_serie + 1
+
+
