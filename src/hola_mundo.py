0c242e96a9d7c654f774299a7e7f2fe3936178e7
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-07-03 12:20:57 -0500

    Imprimir el hola mundo!

---
+++ src/hola_mundo.py
@@ -0,0 +1,16 @@
+"""
+Uso: Imprimir el hola mundo!
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 03 Julio 2021
+"""
+
+print("Hola Mundo")
+
+
+cars = ['Ford', 'BMW', 'Volvo']
+
+
+
+
