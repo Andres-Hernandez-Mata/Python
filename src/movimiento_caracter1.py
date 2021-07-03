a83eb8c72c4ec2e9120f1e0f71296cb7055003eb
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-07-03 12:21:21 -0500

    While

---
+++ src/movimiento_caracter1.py
@@ -0,0 +1,33 @@
+"""
+Uso: While
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 03 Julio 2021
+"""
+
+
+import os, time, sys
+
+op_usr = " "
+pos = 0
+lim = int(input("Espacios: "))
+
+os.system("clear")
+while op_usr == "d" or op_usr == "i" or " ":
+	if op_usr == "d" and pos < lim:
+		pos = pos + 1
+	elif op_usr == "i" and pos > 0:
+		pos = pos - 1
+	elif op_usr == "s":
+		os.system("clear")
+		print("Hasta pronto!")
+		time.sleep(3)
+		sys.exit()
+
+	print(" " * pos + "\U0001f600")
+	op_usr = input("Avanzar |" + str(lim) + "| ¿derecha, izquierda, salir? d/i/s : ")
+	os.system("clear")
+
+
+
