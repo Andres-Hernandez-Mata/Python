508bfd3b1e048f55d03c6c56010b82702334df7f
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-07-03 12:21:13 -0500

    Utilizar los metodos los strings o cadenas

---
+++ src/metodos.py
@@ -0,0 +1,25 @@
+"""
+Uso: Utilizar los metodos los strings o cadenas
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 03 Julio 2021
+"""
+
+alfanumericos = "123HOLA"
+print(alfanumericos.isalnum())
+
+alfabetos = "HOLA"
+print(alfabetos.isalpha())
+
+decimales = "\u0030"
+print(decimales.isdecimal())
+
+digitos = "123"
+print(digitos.isdigit())
+
+impresiones = "Hola Mundo 123"
+print(impresiones.isprintable())
+
+
+
