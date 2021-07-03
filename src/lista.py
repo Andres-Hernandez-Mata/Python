f39baba1ee7992ac0d58814c17afd5f26ab9c809
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-07-03 12:21:04 -0500

    Listas en Python

---
+++ src/lista.py
@@ -0,0 +1,22 @@
+"""
+Uso: Listas en Python
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 03 Julio 2021
+"""
+
+lista = list([])
+print(type(lista))
+n = 0
+m = 0
+
+for n in range(0,20):   
+    m = int(input())
+    lista.append(m)
+print(lista)
+
+
+
+
+
